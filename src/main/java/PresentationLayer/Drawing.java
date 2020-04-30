2b789541d25063b3e4cbdb8904c16ea47c12ff44
MattiBSH <cph-mh829@cphbusiness.dk>
2020-04-30 23:51:23 +0200

    draw klasse shhhh jeg laver ikke noget whuush whuush

--- src/main/java/PresentationLayer/Drawing.java
+++ src/main/java/PresentationLayer/Drawing.java
@@ -10,32 +10,33 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Drawing extends Command {
-    private int area;
-    private int requestID;
-    private String email;
-    private int width;
-    private int length;
-    private String cladding;
-    private boolean rooftype;
-    private String roofmat;
-    private int slopeangle;
-    private int lengthS;
-    private int widthS;
+
 
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException {
+         int area;
+         int requestID;
+         String email;
+         int width;
+         int length;
+         String cladding;
+         boolean rooftype;
+         String roofmat;
+         int slopeangle;
+         int lengthS;
+         int widthS;
 
-        Request r = new Request(requestID,  email,  width,  length,  cladding,  rooftype,  roofmat,  slopeangle,  lengthS,  widthS);
-        int length = r.getLength();
-        int width = r.getWidth();
-        int lengthS = r.getLengthS();
-        int widthS = r.getWidthS();
+        Request r = new Request(1,  "mattemanden@gmail.com",  400,  400,  "oak",  true,  "wood",  40,  400,  400);
+          length = r.getLength();
+          width = r.getWidth();
+          lengthS = r.getLengthS();
+          widthS = r.getWidthS();
 
 
 //in progress...
 
         SVG svg = new SVG(800, 600, "0,0,800,600",0,0);
-        SVG frame = new SVG(900,800,"0,0,900,800",0,0);
+        SVG svgInnerDrawing = new SVG(900,800,"0,0,900,800",0,0);
         svg.addRect(0,0,length,width);
         svg.addRect(0,35,lengthS,widthS);
         svg.addRect(0,565,4,780);
