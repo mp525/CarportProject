a349452edcc24fd145a8f34b86cfa7beca756fb9
Mathias Parking <cph-mp525@cphbusiness.dk>
2020-05-03 14:31:21 +0200

    begyndelse af spær

--- src/main/java/FunctionLayer/StyklisteBeregner.java
+++ src/main/java/FunctionLayer/StyklisteBeregner.java
@@ -6,5 +6,17 @@ public class StyklisteBeregner {
     private ArrayList<Material> matList = new ArrayList();
 
 
+    public static int antalSpær(Request request){
+     int spær = 2;
+        int længde = request.getLength();
+        int bredde = request.getWidth();
 
+        int antal = længde/55;
+        for (int i = 0; i <= længde; i++){
+
+        }
+
+
+        return spær;
+    }
 }
