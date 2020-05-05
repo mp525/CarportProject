230c24066e39e07bb7b1f382534d52f90b000969
LouYourLife <vibekenordestgaard@gmail.com>
2020-05-05 13:40:54 +0200

    styklisteberegner

--- src/main/java/DBAccess/Connector.java
+++ src/main/java/DBAccess/Connector.java
@@ -14,7 +14,8 @@ public class Connector {
 
     private static final String URL = "jdbc:mysql://localhost:3306/carbase?serverTimezone=CET&useSSL=false";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "SwordsAndSandals1337";
+    //private static final String PASSWORD = "SwordsAndSandals1337";
+    private static final String PASSWORD = "AtM12K13BoC185";
 
 
 
--- src/main/java/FunctionLayer/StyklisteBeregner.java
+++ src/main/java/FunctionLayer/StyklisteBeregner.java
@@ -9,80 +9,80 @@ public class StyklisteBeregner {
 
     //private ArrayList<Material> matList = new ArrayList();
 
-    public static int antalSpær(int length){
-        double antal = Math.round((length/55.0)) + 1.0;
+    public static int antalSpær(int length) {
+        double antal = Math.round((length / 55.0)) + 1.0;
         int spær = (int) antal;
         return spær;
     }
 
-     public int stolpeAntal(Request r){
-     //starter med en i hvert hjørne så 4
-     int stolpeAntal=4;
-     int stolpeAntalBcLength = 0;
-     int stolpeAntalBcWidth = 0;
-     int stolpeAntalShed=0;
+    public int stolpeAntal(Request r) {
+        //starter med en i hvert hjørne så 4
+        int stolpeAntal = 4;
+        int stolpeAntalBcLength = 0;
+        int stolpeAntalBcWidth = 0;
+        int stolpeAntalShed = 0;
 
-     int lengths =r.getLengthS();
-     boolean shed=false;
-    if(lengths!=0){
-        shed=true;
-    }
+        int lengths = r.getLengthS();
+        boolean shed = false;
+        if (lengths != 0) {
+            shed = true;
+        }
 //340 højde
-     //set i brochuren... //Forreste stolper skal være 208 cm. Høje og bagerste skal være 202 cm.
-     int width  = r.getWidth();
-     int length = r.getLength();
-     System.out.println(width);
-     System.out.println(length);
-     //bredde
+        //set i brochuren... //Forreste stolper skal være 208 cm. Høje og bagerste skal være 202 cm.
+        int width = r.getWidth();
+        int length = r.getLength();
+        System.out.println(width);
+        System.out.println(length);
+        //bredde
 
-            stolpeAntalBcWidth=width/300;
+        stolpeAntalBcWidth = width / 300;
 
-    // længde
+        // længde
 
-     stolpeAntalBcLength=length/300;
-    stolpeAntalBcLength=stolpeAntalBcLength+stolpeAntalBcLength;
-     //leder videoen første møde havde det her info.. //20 kvadrat meter = 4 stolper // 30 kvadrat meter = 6 stolper
-     //teknisk møde med håndværker gav denne information... //hver tredje meter skal der være en stolpe(i hver side)
+        stolpeAntalBcLength = length / 300;
+        stolpeAntalBcLength = stolpeAntalBcLength + stolpeAntalBcLength;
+        //leder videoen første møde havde det her info.. //20 kvadrat meter = 4 stolper // 30 kvadrat meter = 6 stolper
+        //teknisk møde med håndværker gav denne information... //hver tredje meter skal der være en stolpe(i hver side)
 
-     if (shed==true){
-         //hver hjørne af skuret
+        if (shed == true) {
+            //hver hjørne af skuret
 
-         //hver side af døren ved skuret
-         stolpeAntalShed=6;
-     }
+            //hver side af døren ved skuret
+            stolpeAntalShed = 6;
+        }
 
-     //+ alt
-        stolpeAntal=stolpeAntal+stolpeAntalBcLength+stolpeAntalBcWidth+stolpeAntalShed;
-     return stolpeAntal;
- }
-
-    public void prisStolpe(Request r){
-       int antal = stolpeAntal(r);
-       double pris=antal*3.40*41.95;
+        //+ alt
+        stolpeAntal = stolpeAntal + stolpeAntalBcLength + stolpeAntalBcWidth + stolpeAntalShed;
+        return stolpeAntal;
     }
 
- // @Vibeke
- public void shed() {
-     // Regnestykke: (b*h*2)+(l*h*2)+dørtilbehør
-     // Antagelser:
-     // Dør i meter
-     double doorH = 2.15;
-     double doorW = 1.0;
-     // Dør vil blive placeret på en bredde side
+    public void prisStolpe(Request r) {
+        int antal = stolpeAntal(r);
+        double pris = antal * 3.40 * 41.95;
+    }
 
-     // Skur højde i meter
-     double shedH = 2.5;
+    // @Vibeke
+    public void shed() {
+        // Regnestykke: (b*h*2)+(l*h*2)+dørtilbehør
+        // Antagelser:
+        // Dør i meter
+        double doorH = 2.15;
+        double doorW = 1.0;
+        // Dør vil blive placeret på en bredde side
 
-     // Test mål i meter:
-     double length = 1.80;
-     double width = 2.40;
-     double area = length*width;
+        // Skur højde i meter
+        double shedH = 2.5;
 
-     // Wood for walls:
-     double woodPriceM = 22.95;
-     double woodPriceOne = shedH*woodPriceM;
+        // Test mål i meter:
+        double length = 1.80;
+        double width = 2.40;
+        double area = length * width;
 
-     double wallL1 = length/0.16;
- }
+        // Wood for walls:
+        double woodPriceM = 22.95;
+        double woodPriceOne = shedH * woodPriceM;
+
+        double wallL1 = length / 0.16;
+    }
 
 }
