a63087f6f53246edbb4e5abe385d2e68f0db5cdf
Sjuber <54933648+Sjuber@users.noreply.github.com>
2020-05-05 15:36:57 +0200

    added things for roof calcer

--- src/main/java/FunctionLayer/TagBeregner.java
+++ src/main/java/FunctionLayer/TagBeregner.java
@@ -3,12 +3,9 @@ package FunctionLayer;
 public class TagBeregner {
 
 
-    //private ArrayList<Material> matList = new ArrayList();
 
-    public static double Area(Request req) {
-        double areal = req.getArea();
-        return areal;
-    }
+
+
 
 
     public int numRofPlatesSingls(Request req) {
@@ -53,5 +50,63 @@ public class TagBeregner {
         return Rscrews;
     }
 
+    public int Cal25x200x300FnB(Request req) {
+        // for the front and back stern
+        //Taken from the example of 780 x 600, and here using the 25x200x300
+        int max = 600;
+        int half = max / 2;
+        int quart = half / 2;
+        double w = req.getWidth();
+
+        if (w < quart){
+            return 1;
+        } else if (w >= quart && w <= half){
+            return 2;
+        }else if (w >= half && w <= max){
+            return 4;
+        }else { return 4;
+        }
+    }
+
+
+    public int Cal25x200x360Sides(Request req) {
+        // for the side sterns
+        //Taken from the example of 780 x 600, and here using the 25x200x360
+        int max = 780;
+        int half = max / 2;
+        int quart = half / 2;
+        double l = req.getLength();
+
+        if (l < quart){
+            return 2;
+        } else if (l >= quart && l <= half){
+            return 4;
+        }else if (l >= half && l <= max){
+            return 6;
+        }else
+            {return 6;}
+    }
+
+    public int Cal25x125x300Front(Request req) {
+        // it calculates via a number of 300, because it will measure so it fits the length. Using
+
+        int max = 780;
+        int half = max / 2;
+        int quart = half / 2;
+        double w = req.getWidth();
+        int numbOfPlanks = 0;
+
+        if(w <= 300){
+            numbOfPlanks = 1;
+        } else if (w > 300 && w <= 600){
+            numbOfPlanks = 2;
+        } else if (w>600){
+            numbOfPlanks = 3;
+        }
+        return  numbOfPlanks;
+    }
+
+
 
 }
+
