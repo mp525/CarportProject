78fadbff312cd0af229fbd1d9cfc5df66c69822e
Mathias Parking <cph-mp525@cphbusiness.dk>
2020-05-06 13:00:50 +0200

    materialhelper spær

--- src/main/java/FunctionLayer/MaterialHelper.java
+++ src/main/java/FunctionLayer/MaterialHelper.java
@@ -21,7 +21,35 @@ public class MaterialHelper {
         return slopeSet;
     }
 
-    public static void udregnSpær(ArrayList<Material> stykliste){
+    public static void udregnSpær(ArrayList<Material> stykliste, Request request){
+        int antalSpær = StyklisteBeregner.antalSpær(request.getLength());
+        int spærLængde = StyklisteBeregner.længdeSpær(request.getWidth());
+        double spærPris = StyklisteBeregner.spærPris(spærLængde, antalSpær); //Her er det den samlede pris
+        int antalRemme = 2;
+        if(!(request.getLengthS() == 0)){
+            antalRemme++;
+        }
+        double remPris = StyklisteBeregner.remPris(request.getLength(), antalRemme);
+        double beslagPris = antalSpær * 45.95;
+        int beslagSkrueAntal = StyklisteBeregner.beslagSkrueAntal(request.getLength());
+        double beslagSkruePris = beslagSkrueAntal * 259;
+        //int remLængde = reviewReq.getLengthS(); RemlængdeS er jeg lidt i tvivl om
+        Material spær = new Material("45x195 mm. spærtræ ubh.", "Spær, monteres på rem",
+                "stk", "Træ", spærLængde, spærPris, antalSpær);
+        Material remme = new Material("45x195 mm. spærtræ ubh.", "Remme i sider, sadles ned i stolper",
+                "stk", "Træ", request.getLength(), remPris, antalRemme);
+        Material beslagHøjre = new Material("universal beslag 190mm højre", "Til montering af spær på rem",
+                "stk", "Beslag og skruer", 0, beslagPris, antalSpær);
+        Material beslagVenstre = new Material("universal beslag 190mm venstre", "Til montering af spær på rem",
+                "stk", "Beslag og skruer", 0, beslagPris, antalSpær);
+        Material beslagSkruer = new Material("4,0x50mm. beslagskruer 250 stk.", "Til montering af universalbeslag " +
+                "+ hulbånd", "pakke", "Beslag og skruer", 0, beslagSkruePris, beslagSkrueAntal);
+
+        stykliste.add(spær);
+        stykliste.add(remme);
+        stykliste.add(beslagHøjre);
+        stykliste.add(beslagVenstre);
+        stykliste.add(beslagSkruer);
 
     }
 }
--- src/main/java/PresentationLayer/Review.java
+++ src/main/java/PresentationLayer/Review.java
@@ -1,10 +1,7 @@
 package PresentationLayer;
 
 import DBAccess.RequestMapper;
-import FunctionLayer.LoginSampleException;
-import FunctionLayer.Material;
-import FunctionLayer.Request;
-import FunctionLayer.StyklisteBeregner;
+import FunctionLayer.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -33,36 +30,8 @@ public class Review extends Command {
         request.setAttribute("reviewID", reviewReq.getRequestID());
 
         //Spærudregning
-        int antalSpær = StyklisteBeregner.antalSpær(reviewReq.getLength());
-        int spærLængde = StyklisteBeregner.længdeSpær(reviewReq.getWidth());
-        double spærPris = StyklisteBeregner.spærPris(spærLængde, antalSpær); //Her er det den samlede pris
-        int antalRemme = 2;
-        if(!(reviewReq.getLengthS() == 0)){
-            antalRemme++;
-        }
-        double remPris = StyklisteBeregner.remPris(reviewReq.getLength(), antalRemme);
-        double beslagPris = antalSpær * 45.95;
-        int beslagSkrueAntal = StyklisteBeregner.beslagSkrueAntal(reviewReq.getLength());
-        double beslagSkruePris = beslagSkrueAntal * 259;
-        //int remLængde = reviewReq.getLengthS(); RemlængdeS er jeg lidt i tvivl om
-        Material spær = new Material("45x195 mm. spærtræ ubh.", "Spær, monteres på rem",
-                "stk", "Træ", spærLængde, spærPris, antalSpær);
-        Material remme = new Material("45x195 mm. spærtræ ubh.", "Remme i sider, sadles ned i stolper",
-                "stk", "Træ", reviewReq.getLength(), remPris, antalRemme);
-        Material beslagHøjre = new Material("universal beslag 190mm højre", "Til montering af spær på rem",
-                "stk", "Beslag og skruer", 0, beslagPris, antalSpær);
-        Material beslagVenstre = new Material("universal beslag 190mm venstre", "Til montering af spær på rem",
-                "stk", "Beslag og skruer", 0, beslagPris, antalSpær);
-        Material beslagSkruer = new Material("4,0x50mm. beslagskruer 250 stk.", "Til montering af universalbeslag " +
-                "+ hulbånd", "pakke", "Beslag og skruer", 0, beslagSkruePris, beslagSkrueAntal);
-
-        stykliste.add(spær);
-        stykliste.add(remme);
-        stykliste.add(beslagHøjre);
-        stykliste.add(beslagVenstre);
-        stykliste.add(beslagSkruer);
-
-
+        MaterialHelper.udregnSpær(stykliste, reviewReq);
+        
 
 
 
