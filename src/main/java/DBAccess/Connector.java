230becb4235d005ab9ccf4a5d8661d8cdd7aee8c
MattiBSH <cph-mh829@cphbusiness.dk>
2020-05-13 10:24:34 +0200

    ok

--- src/main/java/DBAccess/Connector.java
+++ src/main/java/DBAccess/Connector.java
@@ -6,19 +6,13 @@ import java.sql.SQLException;
 
 /**
  The purpose of Connector is to...
-
  @author kasper
  */
 public class Connector {
 
-
-    private static final String URL = "jdbc:mysql://localhost:3306/carbase?serverTimezone=CET&useSSL=false";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "cph39178";
-
-    //private static final String PASSWORD = "SwordsAndSandals1337";
-    //private static final String PASSWORD = "AtM12K13BoC185";
-
+    private static String URL;
+    private static String USERNAME;
+    private static String PASSWORD;
 
     private static Connection singleton;
 
@@ -28,10 +22,28 @@ public class Connector {
 
     public static Connection connection() throws ClassNotFoundException, SQLException {
         if ( singleton == null ) {
+            setDBCredentials();
             Class.forName( "com.mysql.cj.jdbc.Driver" );
             singleton = DriverManager.getConnection( URL, USERNAME, PASSWORD );
         }
         return singleton;
+    }
+
+    public static void setDBCredentials() {
+        String deployed = System.getenv("DEPLOYED");
+        if (deployed != null) {
+            //Prod: hent variabler fra setenv.sh
+            URL = System.getenv("JDBC_CONNECTION_STRING");
+            USERNAME = System.getenv("JDBC_USER");
+            PASSWORD = System.getenv("JDBC_PASSWORD");
+
+        } else {
+            //Localhost
+            URL = "jdbc:mysql://localhost:3306/carbase?serverTimezone=CET&useSSL=false";
+            USERNAME = "root";
+            PASSWORD = "Matti12345h";
+
+        }
     }
 
 }
