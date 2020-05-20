4f16daf4a91fa0d22be56d0f4d8aa220ecd3385a
Mathias Parking <cph-mp525@cphbusiness.dk>
2020-05-20 12:58:05 +0200

    just before pull af javadoc niko

--- src/main/java/DBAccess/UserMapper.java
+++ src/main/java/DBAccess/UserMapper.java
@@ -19,7 +19,7 @@ public class UserMapper {
     public static void createUser( User user ) throws LoginSampleException {
         try {
             Connection con = Connector.connection();
-            String SQL = "INSERT INTO Users (email, password, role) VALUES (?, ?, ?)";
+            String SQL = "INSERT INTO carbase.users (email, password, role) VALUES (?, ?, ?)";
             PreparedStatement ps = con.prepareStatement( SQL, Statement.RETURN_GENERATED_KEYS );
             ps.setString( 1, user.getEmail() );
             ps.setString( 2, user.getPassword() );
